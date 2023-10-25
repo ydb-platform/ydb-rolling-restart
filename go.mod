20bcd984f364aca9f3a8ea5d79008547e789324a
ArtemTrofimushkin <artemtrofimushkin@gmail.com>
2023-10-25 13:38:40 +0300

    feat: add logging

--- go.mod
+++ go.mod
@@ -2,17 +2,11 @@ module github.com/ydb-platform/ydb-rolling-restart
 
 go 1.20
 
-require github.com/ydb-platform/ydb-go-genproto v0.0.0-20230427174428-8de82b82d668
-
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.7.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
-	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
-	golang.org/x/text v0.3.3 // indirect
-	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
-	google.golang.org/grpc v1.47.0 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	github.com/spf13/cobra v1.7.0
+	github.com/spf13/pflag v1.0.5
+	go.uber.org/multierr v1.11.0
+	go.uber.org/zap v1.26.0
 )
+
+require github.com/inconshreveable/mousetrap v1.1.0 // indirect
--- main.go
+++ main.go
@@ -8,19 +8,46 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/ydb-platform/ydb-rolling-restart/cmd"
+	"go.uber.org/zap/zapcore"
+
+	"go.uber.org/zap"
 )
 
+func createLogger(level string) (zap.AtomicLevel, *zap.Logger) {
+	atom, _ := zap.ParseAtomicLevel(level)
+	encoderCfg := zap.NewProductionEncoderConfig()
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	return atom, zap.New(zapcore.NewCore(
+		zapcore.NewConsoleEncoder(encoderCfg),
+		zapcore.Lock(os.Stdout),
+		atom,
+	))
+}
+
 func main() {
-	var root = &cobra.Command{
+	logLevel := "info"
+	logLevelSetter, logger := createLogger(logLevel)
+	root := &cobra.Command{
 		Use:   "ydb-rolling-restart",
 		Short: "The rolling restart utility",
 		Long:  "The rolling restart utility (long version)",
+		PersistentPreRun: func(_ *cobra.Command, _ []string) {
+			lvc, err := zapcore.ParseLevel(logLevel)
+			if err != nil {
+				logger.Warn("Failed to set level")
+				return
+			}
+			logLevelSetter.SetLevel(lvc)
+		},
 	}
+	defer logger.Sync()
 
+	root.PersistentFlags().StringVarP(&logLevel, "log-level", "", logLevel, "Logging level")
 	root.AddCommand(
-		cmd.NewServicesCommand(),
-		cmd.NewTenantsCommand(),
-		cmd.NewRestartCommand(),
+		cmd.NewServicesCommand(logger),
+		cmd.NewTenantsCommand(logger),
+		cmd.NewRestartCommand(logger),
 	)
 
 	if err := root.Execute(); err != nil {

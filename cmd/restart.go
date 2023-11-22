10808fa003782db5e24905c59936b96a5e2f619e
ArtemTrofimushkin <artemtrofimushkin@gmail.com>
2023-11-22 16:26:10 +0300

    fix: add draft for restart cmd

--- cmd/restart.go
+++ cmd/restart.go
@@ -3,15 +3,32 @@ package cmd
 import (
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
+
+	"github.com/ydb-platform/ydb-rolling-restart/pkg/options"
 )
 
-func NewRestartCommand(logger *zap.Logger) *cobra.Command {
-	return &cobra.Command{
+type RestartOptions struct {
+	CMS  *options.CMS
+	GRPC *options.GRPC
+}
+
+func NewRestartCommand(lf *zap.Logger) *cobra.Command {
+	logger := lf.Sugar()
+	opts := RestartOptions{
+		CMS:  &options.CMS{},
+		GRPC: &options.GRPC{},
+	}
+	cmd := &cobra.Command{
 		Use:   "restart",
 		Short: "Perform restart of YDB cluster",
 		Long:  "Perform restart of YDB cluster (long version)",
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			logger.Info("restart called")
+			return nil
 		},
 	}
+
+	opts.CMS.DefineFlags(cmd.Flags())
+	opts.GRPC.DefineFlags(cmd.Flags())
+	return cmd
 }
